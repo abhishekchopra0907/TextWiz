2c4d28fb6ab757d9de153135031b674a1280f971
shek <abhishekchopr0907@gmail.com>
2020-07-14 12:40:18 +0530

    working

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Dependencies
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
+
 from sklearn.externals import joblib
 import traceback
 import pandas as pd 
@@ -46,7 +47,7 @@ for line in f:
     embeddings_index[word] = coefs
 f.close()
 
-def read(data):
+def read_p(data):
     text= data.split(". ")
     sentences = []
 #         for i,sent in text:
@@ -54,7 +55,6 @@ def read(data):
         sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
 #             print(sentence)
     sentences.pop() 
-    
     return sentences
 
 def vectorizer(summ_data,embeddings_index):
@@ -105,16 +105,17 @@ def dist_bw_sent_doc_cos(vec1,vec2):
     return dist_arr
 
 def summarizer(doc_norm2_dist,joined_summ):
-    summarize_text = []
-    meaner=[]
-    for i in range(1,len(doc_norm2_dist)+1):
-        meaner.append(doc_norm2_dist[i])
-    mean_arr=np.array(meaner)
-    threshold=(mean_arr.mean()+np.median(mean_arr))/2.2
-    for i in range(1,len(doc_norm2_dist)+1):
-        if doc_norm2_dist[i]<threshold:
-             summarize_text.append("".join(joined_summ[i]))
-    return summarize_text
+	summarize_text = []
+	meaner=[]
+	for i in range(1,len(doc_norm2_dist)+1):
+		meaner.append(doc_norm2_dist[i])
+	mean_arr=np.array(meaner)
+	threshold=(mean_arr.mean()+np.median(mean_arr))/1.8
+	# threshold=100
+	for i in range(1,len(doc_norm2_dist)+1):
+		if doc_norm2_dist[i]<threshold:
+			summarize_text.append("".join(joined_summ[i]))
+	return summarize_text
 
 def corpus_maker(summarize_text):
     corpus = []
@@ -178,138 +179,148 @@ def get_top_n3_words(corpus, n=None):
     return words_freq[:n]
 
 def generate_summ_key(path):
-    summ_data=read(path)
-    joined_summ={}
-    n=0
-    
-    for list_w in summ_data:
-        n=n+1
-        text=' '.join(list_w)
-        joined_summ[n]=text
-        
-    summ_vec=vectorizer(summ_data,embeddings_index)
-    summ_sent_vec=word_binder_dict(summ_vec)
-    doc_vec=doc_binder(summ_sent_vec)
-    doc_cos_dist=dist_bw_sent_doc_cos(summ_sent_vec,doc_vec)
-    doc_norm2_dist=dist_bw_sent_doc_norm2(summ_sent_vec,doc_vec)
-    
-    
-    summarize_text=summarizer(doc_norm2_dist,joined_summ)
-        
-    stop_words = set(stopwords.words("english"))
-        
-    corpus=corpus_maker(summarize_text)
-    cv=CountVectorizer(max_df=1,stop_words=stop_words, max_features=10000, ngram_range=(1,3))
-    X=cv.fit_transform(corpus)
-    
-    n=5
-    
-    top_words = get_top_n_words(corpus, n)
-    top_df = pd.DataFrame(top_words)
-    top_df.columns=["Word", "Freq"]
-    
-    keyword=[]
-    
-    for word in top_df['Word']:
-        keyword.append(word)
-    
-    top2_words = get_top_n2_words(corpus, n)
-    top2_df = pd.DataFrame(top2_words)
-    top2_df.columns=["Bi-gram", "Freq"]
-    top2_df['Bi-gram']
-    
-    bi=[]
-    
-    for word in top2_df['Bi-gram']:
-        keyword.append(word)
-   
-    top3_words = get_top_n3_words(corpus, n)
-    top3_df = pd.DataFrame(top3_words)
-    top3_df.columns=["Tri-gram", "Freq"]
-    
-    tri=[]
-    
-    for word in top3_df['Tri-gram']:
-        keyword.append(word)
-        
-    return summarize_text,keyword
-@app.route("/")
-def index():
-    return render_template("index.html")
-@app.route('/list_add', methods=['POST'])
+	summ_data= read_p(path)
+	joined_summ={}
+	n=0
+
+	for list_w in summ_data:
+		n=n+1
+		text=' '.join(list_w)
+		joined_summ[n]=text
+	summ_vec=vectorizer(summ_data,embeddings_index)
+	summ_sent_vec=word_binder_dict(summ_vec)
+	doc_vec=doc_binder(summ_sent_vec)
+	doc_cos_dist=dist_bw_sent_doc_cos(summ_sent_vec,doc_vec)
+	doc_norm2_dist=dist_bw_sent_doc_norm2(summ_sent_vec,doc_vec)
+
+	summarize_text=summarizer(doc_norm2_dist,joined_summ)
+		
+	stop_words = set(stopwords.words("english"))
+		
+	corpus=corpus_maker(summarize_text)
+	cv=CountVectorizer(max_df=1,stop_words=stop_words, max_features=10000, ngram_range=(1,3))
+	X=cv.fit_transform(corpus)
+
+	n=5
+
+	top_words = get_top_n_words(corpus, n)
+	top_df = pd.DataFrame(top_words)
+	top_df.columns=["Word", "Freq"]
+
+	keyword=[]
+
+	for word in top_df['Word']:
+		keyword.append(word)
+
+	top2_words = get_top_n2_words(corpus, n)
+	top2_df = pd.DataFrame(top2_words)
+	top2_df.columns=["Bi-gram", "Freq"]
+	top2_df['Bi-gram']
+
+	bi=[]
+
+	for word in top2_df['Bi-gram']:
+		keyword.append(word)
+
+	top3_words = get_top_n3_words(corpus, n)
+	top3_df = pd.DataFrame(top3_words)
+	top3_df.columns=["Tri-gram", "Freq"]
+
+	tri=[]
+
+	for word in top3_df['Tri-gram']:
+		keyword.append(word)
+		
+	return summarize_text,keyword
+# @app.route("/")
+# def index():
+#     return render_template("index.html")
+@app.route('/list_add', methods=['GET','POST'])
 def list_add():
-    print("Before JSON")
-    text = request.get_json()
-    print(text)
-    r=text['data']
-    print("This is where i am")
-    sys.stdout.flush()
-    a,b=generate_summ_key(r)
-    response = google_images_download.googleimagesdownload() 
+	# return "hello"
+    #print("Before JSON")
+	text = request.get_json()
+	# print(text)
+	# r="The tiger is the largest extant cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange-brown fur with a lighter underside. It is an apex predator, primarily preying on ungulates such as deer and wild boar."
+	r=text['data']
+	# print(r)
+    #print("This is where i am")
+    #sys.stdout.flush()
+	a,b=generate_summ_key(r)
+	response = google_images_download.googleimagesdownload() 
+	search_queries = []
 
-    search_queries = []
-
-    for contents in b:
-        search_queries.append(contents)
+	for contents in b:
+		search_queries.append(contents)
 
 
 
-    def downloadimages(query): 
-        # keywords is the search query 
-        # format is the image file format 
-        # limit is the number of images to be downloaded 
-        # print urs is to print the image file url 
-        # size is the image size which can 
-        # be specified manually ("large, medium, icon") 
-        # aspect ratio denotes the height width ratio 
-        # of images to download. ("tall, square, wide, panoramic") 
-        arguments = {"keywords": query, 
-                    "format": "jpg", 
-                    "limit":1, 
-                    "print_urls":True, 
-                    "size": "medium", 
-                    "aspect_ratio": "panoramic"} 
-        try: 
-            response.download(arguments) 
-        
-        # Handling File NotFound Error   
-        except FileNotFoundError: 
-            arguments = {"keywords": query, 
-                        "format": "jpg", 
-                        "limit":1, 
-                        "print_urls":True, 
-                        "size": "medium"} 
-                        
-            # Providing arguments for the searched query 
-            try: 
-                # Downloading the photos based 
-                # on the given arguments 
-                response.download(arguments)
-            except: 
-                pass
+	def downloadimages(query): 
+		# keywords is the search query 
+		# format is the image file format 
+		# limit is the number of images to be downloaded 
+		# print urs is to print the image file url 
+		# size is the image size which can 
+		# be specified manually ("large, medium, icon") 
+		# aspect ratio denotes the height width ratio 
+		# of images to download. ("tall, square, wide, panoramic") 
+		arguments = {"keywords": query, 
+					"format": "jpg", 
+					"limit":1, 
+					"print_urls":True, 
+					"size": "medium", 
+					"aspect_ratio": "panoramic"} 
+		try: 
+			response.download(arguments) 
+		
+		# Handling File NotFound Error   
+		except FileNotFoundError: 
+			arguments = {"keywords": query, 
+						"format": "jpg", 
+						"limit":1, 
+						"print_urls":True, 
+						"size": "medium"} 
+						
+			# Providing arguments for the searched query 
+			try: 
+				# Downloading the photos based 
+				# on the given arguments 
+				response.download(arguments)
+			except: 
+				pass
 
-    # Driver Code 
-    # orig_stdout = sys.stdout
-    # f = open('purang_dega.txt', 'w+')
-    # sys.stdout = f
+	# Driver Code 
+	# orig_stdout = sys.stdout
+	# f = open('purang_dega.txt', 'w+')
+	# sys.stdout = f
+	# print (1234)
+	links=[]
+	for query in search_queries:
+		downloadimages(query)
+		links.append(query)
+	f = open("keywords.txt", "r")
+	
+	# print (1234)
+	# if f.mode=="r":
+		# contents = f.read()
+		# links.append(contents)
+	p=''
+	# print (1234)
+	for i in a:
+		# p = i + '.'+''.join(prev)
+		p=p+'.'+i
+	output= p+'  %42069420%  ' + ' '.join(links)
+	# print (1234)
+	return output
 
-    for query in search_queries:
-        downloadimages(query)
-    f = open("keywords.txt", "r")
-    links=[]
-    if f.mode=="r":
-        contents = f.read()
-        links.append(contents)
-    prev=''
-    for i in a:
-        p = i + '.'+''.join(prev)
-        prev=i
-    output= p+'  %42069420%  ' + links[0]
-    return output,render_template("list_add.html")
-
+# @app.route('/wow', methods=['POST'])
+# def wow():
+# 	a=[1,2,3]
+# 	return "a"
 
 if __name__ == "__main__":
-    app.run(host='localhost', debug=True)
+    app.debug=True
+#     app.run()
 
 
 
